c2d3e65a58acbc807cf275996bca9a66f8c14ca9
Munaf Hajir <munafhajirpro@gmail.com>
2018-02-13 11:29:44 +0530

    Upload By Munaf

---
+++ gfm.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2018 at 06:57 AM
+-- Server version: 5.6.17-log
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `gfm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `enroll_no` int(10) NOT NULL,
+  `unique_id` int(4) NOT NULL,
+  `dept` varchar(255) NOT NULL,
+  `class_coordinator` varchar(255) NOT NULL,
+  `gfm` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `attend1` int(11) DEFAULT NULL,
+  `attend2` int(11) DEFAULT NULL,
+  `attend3` int(11) DEFAULT NULL,
+  `attend4` int(11) DEFAULT NULL,
+  `overall` int(11) DEFAULT NULL,
+  `ptt1` int(11) DEFAULT NULL,
+  `ptt2` int(11) DEFAULT NULL,
+  `pst` int(11) DEFAULT NULL,
+  `m_result` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `enroll_no` (`enroll_no`,`unique_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `validation`
+--
+
+CREATE TABLE IF NOT EXISTS `validation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
