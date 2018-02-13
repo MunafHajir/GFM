ee45fd6a4a2fab7dc6a2e555efd03fdfb40d0c14
sulemankhan447 <sulemankhan447@gmail.com>
2018-02-13 12:54:11 +0530

    Bug Fixed and Updated

--- loginProcess.php
+++ loginProcess.php
@@ -5,32 +5,38 @@
 			$username = $_POST['username'];
 			$password = $_POST['password'];
 			$loginrole = $_POST['loginrole'];
-			$sql = "select * from validation where username='$username' and password='$password' and type='loginrole'";
+			$sql = "select * from student where enroll_no='$username' and password='$password'";
 			$results = mysqli_query($con,$sql) or die(mysqli_error($con));
 			if(mysqli_num_rows($results)>0)
 			{
-				$row = mysqli_fetch_array($results);
-				if(strcmp($row['type'],"student")==0)
-				{
 					header("Location:sDashboard.php");	//student dashboard
 					$_SESSION['user'] = $username;
-				}	
-				else if(strcmp($row['type'],"cordinator") == 0)
-				{
-					header("Location:cDashboard.php");	//cordinator dashboard
-					$_SESSION['user'] = $username;
-				}
-				else
-				{	
-					header("Location:gDashboard.php");	//gfm dashboard
-					$_SESSION['user'] = $username;
-				}
 			}
 			else
 			{
-				echo "<script>alert('Invalid Username or Password');</script>";
-				header("Refresh: 0; url=index.html");
-			}
+				$sql = "select * from validation where username='$username' and password='$password' and type='$loginrole'";
+				$results = mysqli_query($con,$sql) or die(mysqli_error($con));
+				if(mysqli_num_rows($results)>0)
+				{
+					$row = mysqli_fetch_array($results);
+					if(strcmp($row['type'],"cordinator") == 0)
+					{
+						header("Location:cDashboard.php");	//cordinator dashboard
+						$_SESSION['user'] = $username;
+					}
+					else
+					{	
+						header("Location:gDashboard.php");	//gfm dashboard
+						$_SESSION['user'] = $username;
+					}
+				}
+				else
+				{
+					echo "<script>alert('Invalid Username or Password');</script>";
+					header("Refresh: 0; url=index.html");
+				}
+			}//end of student else
+
 	}
 	else
 	{
