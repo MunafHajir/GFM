961182ed534c9590ec2a0d0229e99e033826faed
Munaf Hajir <munafhajirpro@gmail.com>
2018-03-30 15:02:55 +0530

    Use GFM.sql some minor changes

--- gfm.sql
+++ gfm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 13, 2018 at 01:25 PM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.27-1+ubuntu16.04.1+deb.sury.org+1
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2018 at 11:31 AM
+-- Server version: 5.6.17-log
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `GFM`
+-- Database: `gfm`
 --
 
 -- --------------------------------------------------------
@@ -26,8 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `student`
 --
 
-CREATE TABLE `student` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `student` (
   `name` varchar(255) NOT NULL,
   `enroll_no` int(10) NOT NULL,
   `unique_id` int(4) NOT NULL,
@@ -44,16 +43,17 @@ CREATE TABLE `student` (
   `ptt2` int(11) DEFAULT NULL,
   `pst` int(11) DEFAULT NULL,
   `m_result` int(11) DEFAULT NULL,
-  `shift` varchar(50) NOT NULL
+  `shift` varchar(255) NOT NULL,
+  PRIMARY KEY (`enroll_no`),
+  UNIQUE KEY `enroll_no` (`enroll_no`,`unique_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `name`, `enroll_no`, `unique_id`, `dept`, `class_coordinator`, `gfm`, `password`, `attend1`, `attend2`, `attend3`, `attend4`, `overall`, `ptt1`, `ptt2`, `pst`, `m_result`, `shift`) VALUES
-(3, 'Moin Gadkari', 1305690017, 6646, 'CO', 'suleman', 'test', '06a998cdd13c50b7875775d4e7e9fa74', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1st Shift'),
-(5, 'Suleman Khan', 1655151515, 1551, 'CO', 'suleman', 'munaf ', '8f375f7f55ba6b6516364168a6cbf0fb', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1st Shift');
+INSERT INTO `student` (`name`, `enroll_no`, `unique_id`, `dept`, `class_coordinator`, `gfm`, `password`, `attend1`, `attend2`, `attend3`, `attend4`, `overall`, `ptt1`, `ptt2`, `pst`, `m_result`, `shift`) VALUES
+('iMH', 1316080154, 1224, 'ME', 'Umar Mursal', 'Umar Mursal', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1st Shift');
 
 -- --------------------------------------------------------
 
@@ -61,54 +61,24 @@ INSERT INTO `student` (`id`, `name`, `enroll_no`, `unique_id`, `dept`, `class_co
 -- Table structure for table `validation`
 --
 
-CREATE TABLE `validation` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `validation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `type` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `type` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `validation`
 --
 
 INSERT INTO `validation` (`id`, `username`, `password`, `type`) VALUES
-(1, 'Moin ', 'moin', 'coordinator'),
-(2, 'munaf ', 'munaf', 'gfm'),
-(3, 'suleman', 'suleman', 'coordinator'),
-(4, 'test', 'test', 'gfm');
+(1, 'Umar Mursal', 'umar123', 'cordinator'),
+(2, 'Umar Mursal', 'umar123', 'gfm'),
+(3, 'Mudassir', '123456', 'gfm'),
+(4, 'Mudassir', '123456', 'cordinator');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `student`
---
-ALTER TABLE `student`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `enroll_no` (`enroll_no`,`unique_id`);
-
---
--- Indexes for table `validation`
---
-ALTER TABLE `validation`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `student`
---
-ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `validation`
---
-ALTER TABLE `validation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
